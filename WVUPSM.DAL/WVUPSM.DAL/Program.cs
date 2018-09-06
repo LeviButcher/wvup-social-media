85b4c51bb9f154e8068b379d42f9ccd4c9b292fb
Levi Butcher <lbutche3@wvup.edu>
2018-09-06 11:04:28 -0400

    DAL innit

---
+++ WVUPSM.DAL/WVUPSM.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WVUPSM.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WVUPSM.DAL/WVUPSM.Models/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WVUPSM.Models
+{
+    public class Class1
+    {
+
+    }
+}

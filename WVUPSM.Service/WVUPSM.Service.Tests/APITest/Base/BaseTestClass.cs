1cf768b0c05d16f7afd60d65e8e1a17b0f06538f
LeviButcher <lbutche3@wvup.edu>
2018-09-09 17:46:03 -0400

    Init service layer test

---
+++ WVUPSM.Service/WVUPSM.Service.Tests/APITest/Base/BaseTestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WVUPSM.Service.Tests.APITest.Base
+{
+    public abstract class BaseTestClass : IDisposable
+    {
+        protected string ServiceAddress = "http://localhost:51117/";
+        protected string RootAddress = String.Empty;
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
---
+++ WVUPSM.Service/WVUPSM.Service.Tests/APITest/UserControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using WVUPSM.Models.Entities;
+using WVUPSM.Service.Tests.APITest.Base;
+using Xunit;
+
+namespace WVUPSM.Service.Tests.APITest
+{
+    [Collection("{Service Testing}")]
+    public class UserControllerTests : BaseTestClass
+    {
+        public UserControllerTests()
+        {
+            RootAddress = "api/User";
+        }
+
+        [Fact]
+        public async void CreateUser()
+        {
+            using(var client = new HttpClient())
+            {
+                User user = new User()
+                {
+                    Email = "lbutche3@wvup.edu",
+                    UserName = "lbutche"
+                };
+                var userContent = JsonConvert.SerializeObject(user);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(userContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                
+                var response = await client.PostAsync("http://localhost:51117/api/User/Create/Develop@90", byteContent);
+                Assert.True(response.IsSuccessStatusCode);
+            }
+        }
+    }
+}

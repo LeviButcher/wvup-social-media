9d47f936d6ee8b86ea1845549ff9e1b7be373183
Samantha <sburkey@wvup.edu>
2018-09-20 12:19:34 -0400

    Set role of user or admin to users on site

---
+++ WVUPSM.DAL/WVUPSM.DAL/Repos/Interfaces/IRoleRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WVUPSM.Models.Entities;
+
+namespace WVUPSM.DAL.Repos.Interfaces
+{
+    public interface IRoleRepo
+    {
+        IEnumerable<IdentityRole> GetRoles();
+
+        Task<IEnumerable<User>> GetAdmins();
+
+        Task<IEnumerable<User>> GetUsers();
+
+        Task<bool> AddToRoleById(string userId, string roleId);
+
+        Task<bool> AddToUsers(string userId);
+
+        Task<bool> AddToAdmin(string userId);
+    }
+}

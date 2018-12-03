3b77d2ca70665bc72cbe6c91b42385644d5b228c
Levi Butcher <lbutche3@wvup.edu>
2018-12-03 15:56:09 -0500

    Comment on any post screen

---
+++ WVUPSM.MVC/WVUPSM.MVC/wwwroot/js/Comment.js
@@ -0,0 +1,9 @@
+﻿let buttons = document.querySelectorAll('[data-post]');
+buttons.forEach(button => button.addEventListener('click', toggleCommentForm));
+
+function toggleCommentForm() {
+    console.dir(this);
+    let post = this.dataset.post;
+    let commentForm = document.querySelector(`#comment-form${post}`);
+    commentForm.classList.toggle('post-comment-form-active');
+}

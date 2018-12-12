8395b665e095be75fb6e600f4f22fbc9f55f7ef8
LeviButcher <lbutche3@wvup.edu>
2018-12-12 10:41:56 -0500

    Display helpful text when follow feed is empty

--- WVUPSM.MVC/WVUPSM.MVC/ViewComponents/PostFeedViewComponent.cs
+++ WVUPSM.MVC/WVUPSM.MVC/ViewComponents/PostFeedViewComponent.cs
@@ -29,20 +29,24 @@ namespace WVUPSM.MVC.ViewComponents
         {
             IList<UserPost> posts = null;
             ViewData["user-id"] = userId;
+            ViewData["No-Post-Message"] = "No Posts";
             if (myPosts)
             {
                 ViewData["post-call"] = "Post/GetPosts";
+                ViewData["No-Post-Message"] = "This user has no posts";
                 posts = await _webApiCalls.GetMyPostAsync(userId);
             }
             else if(groupId != -1)
             {
                 ViewData["user-id"] = groupId;
                 ViewData["post-call"] = "Group/Posts";
+                ViewData["No-Post-Message"] = "This group has no posts";
                 posts = await _webApiCalls.GetGroupPostsAsync(groupId);
             }
             else
             {
                 ViewData["post-call"] = "Home/GetFollowingPost";
+                ViewData["No-Post-Message"] = "No one you Follow has posted, type a name in the search bar above and find someone";
                 posts = await _webApiCalls.GetFollowingPostAsync(userId);
             }
             return View(posts);
